e7dc4590081778ec255a4c81ae66bf73bc0be5d6
Aesha Justin <109031204+aesha-j25@users.noreply.github.com>
2024-04-21 14:22:28 +0100

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,12 @@
+#request user to input name
+#store input as name
+#output name
+#request user to input age
+#store input as age
+#output age
+#output "Hello World!"
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+print(name)
+print(age)
+print("Hello World!")
